2759e9a6edb9f67ca2f3b42e53d306481a9d00cf
Alexander Ostrowski <ao11@illinois.edu>
2023-07-25 19:59:58 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var helloWorldDiv = document.getElementById('helloWorldContainer');
+    helloWorldDiv.innerHTML = '<h1>Hello, World!</h1>';
+});
