0645f51648d9080659a1b85c735b3237d0f2be91
Alexander Ostrowski <ao11@illinois.edu>
2023-07-25 21:03:46 -0500

    Testing boilerplate d3

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var helloWorldDiv = document.getElementById('helloWorldContainer');
-    helloWorldDiv.innerHTML = '<h1>Hello, World!</h1>';
-});
+// main.js - Your D3.js code goes here
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+// Declare the chart dimensions and margins.
+const width = 640;
+const height = 400;
+const marginTop = 20;
+const marginRight = 20;
+const marginBottom = 30;
+const marginLeft = 40;
+
+// Declare the x (horizontal position) scale.
+const x = d3.scaleUtc()
+    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+    .range([marginLeft, width - marginRight]);
+
+// Declare the y (vertical position) scale.
+const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - marginBottom, marginTop]);
+
+// Create the SVG container.
+const svg = d3.create("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Add the x-axis.
+svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x));
+
+// Add the y-axis.
+svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y));
+
+// Function to append the SVG element to the container.
+function appendSVG(containerElement) {
+    containerElement.append(svg.node());
+}
+
+// Get the container element by ID and call the function.
+const container = document.getElementById("container");
+appendSVG(container);
